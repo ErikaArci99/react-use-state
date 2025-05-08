fca3c2fc2a17f0113ffb7d6e2b7b94b6714a4ca2
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-08 14:42:26 +0200

    [UPD] aggiunto header

---
+++ react-use-state/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='container-header'>
+            <h1>Learn Web Development</h1>
+        </div>
+    )
+}
+
+export default Header
