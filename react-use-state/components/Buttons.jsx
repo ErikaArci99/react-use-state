59dc3fecdcae876f6c020bdd9ec9633c9de2ecea
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-08 14:58:19 +0200

    [UPD] creato bottone statico e stilizzato

---
+++ react-use-state/components/Buttons.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Buttons = () => {
+    return (
+        <div className='container-buttons d-flex justify-content-center'>
+            <button>
+                <h2>HTML</h2>
+            </button>
+        </div>
+    )
+}
+
+export default Buttons
---
+++ react-use-state/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Buttons from './Buttons'
+
+const Main = () => {
+    return (
+        <div className='d-flex justify-content-center'>
+            <div className="container-main">
+                <Buttons />
+            </div>
+        </div>
+    )
+}
+
+export default Main
